97132008f7b6faa380d375ab055ba4da399f7437
Elman Aliev <elman3605@yandex.ru>
2022-06-13 18:17:08 +0500

    feat: add MainApi

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,7 +1,11 @@
 import { FilterCheckbox } from '../FilterCheckbox/FilterCheckbox';
 import './SearchForm.css';
+import { useInput } from '../../hooks/useInput';
 
 export const SearchForm = () => {
+
+    const search = useInput('', { isEmpty: true });
+    if (search.isDirty && search.isError) {search.value = 'Нужно ввести ключевое слово'}
 
     return (
         <div className="search">
@@ -12,15 +16,19 @@ export const SearchForm = () => {
                         name="search-input"
                         className="search__input"
                         placeholder="Фильм"
+                        value={search.value}
+                        onChange={e => search.onChange(e)}
+                        onBlur={e => search.onBlur(e)}
                     />
 
                     <button className="search__button" />
+                    {/* {(search.isDirty && search.isError) && <span>{search.errorText}</span>} */}
                 </div>
 
                 <div className="search__checkbox">
 
 
-                    
+
 
                     <p className="search__checkbox-name">
                         Короткометражки
